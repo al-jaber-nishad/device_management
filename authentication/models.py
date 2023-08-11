f3b43415a2778911f5fad87f449a0aae3c96d94d
al-jaber-nishad <mailnishad02@gmail.com>
2023-08-11 21:13:32 +0600

    Add user registration, login, and profile view features

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class CustomUser(User):
+    
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('regular', 'Regular'),
+    )
+    image = models.ImageField(upload_to='media/', null=True, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='regular')
+
+    class Meta:
+        ordering = ['-id',]
+
+    def __str__(self):
+        return f"{self.username}"
